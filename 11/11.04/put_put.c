a57ebf0a8dece3854e013bf78c7e2f6a5c8d49c3
DraculaAugustus <DraculaAugustus@126.com>
2023-05-11 16:22:53 +0800

    improve put_put.c

--- 11/11.04/put_put.c
+++ 11/11.04/put_put.c
@@ -11,3 +11,24 @@ int main(void)
 
 	return 0;
 }
+
+void put1(const char *string)
+{
+	while(*string)
+		putchar(*string++);
+}
+
+int put2(const char *string)
+{
+	int count = 0;
+	
+	while(*string)
+	{
+		putchar(*string++);
+		count++;
+	}
+
+	putchar('\n');
+
+	return count;
+}
