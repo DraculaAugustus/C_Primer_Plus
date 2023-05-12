35bd207ef0163813d5484b967aa561f51c679535
DraculaAugustus <DraculaAugustus@126.com>
2023-05-12 13:21:06 +0800

    add compare.c nogo.c quit_chk.c

---
+++ 11/11.06/compare.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+#define ANSWER		"Grant"
+#define SIZE		40
+char * s_gets(char * str, int n);
+
+int main(void)
+{
+	char try[SIZE];
+
+	puts("Who is buried in Grant's tomb?");
+	s_gets(try, SIZE);
+	while(strcmp(try, ANSWER))
+	{
+		puts("No, that's wrong. Try again.");
+		s_gets(try, SIZE);
+	}
+	puts("That's right!");
+
+	return 0;
+}
+
+char * s_gets(char * str, int n)
+{
+	char * ret_val;
+	int i = 0;
+
+	ret_val = fgets(str, n, stdin);
+	if(ret_val)
+	{
+		while(str[i] != '\n' && str[i] != '\0')
+			i++;
+		if(str[i] == '\n')
+			str[i] = '\0';
+		else
+			while(getchar() != '\n')
+				continue;
+	}
+	return ret_val;
+}
